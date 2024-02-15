557772444037400690a699c693ea3b558fce6465
aligonzalezlobo <91560733+aligonzalezlobo@users.noreply.github.com>
2024-02-15 09:14:18 +0000

    commiting version 2

--- handover.py
+++ handover.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 def load_data(file_path):
     """
@@ -9,7 +10,8 @@ def load_data(file_path):
 
 def main():
     # Load the dataset
-    data = load_data("final_data.csv")
+    data = load_data("final_data2.csv")
+    time_data = load_data("top_client_data.csv")
 
     # Set page configuration
     st.set_page_config(
@@ -22,28 +24,61 @@ def main():
     pd.set_option("styler.render.max_elements", 5989816)
 
     # Create sidebar navigation for tabs
-    tab = st.sidebar.radio("Navigation", ["Home Page", "Client Ranking", "Client Deep-dive"])
+    tab = st.sidebar.radio("Navigation", ["Home Page", "AI Client Ranking", "Client Deep-dive","Financial Tool"])
+
+    # At the bottom of the sidebar
+    with st.sidebar:
+        # Create an empty space, pushing the content to the bottom
+        for _ in range(100):  # You can adjust the range number to push the logo down as needed
+            st.empty()
+
+        # Center the image by using column width or specifying the width in pixels
+        logo_image2 = "images/image2.png"  # Replace with your image path
+        st.image(logo_image2, use_column_width=True)
+
 
     if tab == "Home Page":
-        # Add your content for the home page
-        st.title("Welcome to Sales Team Tool CLIENTCO")
-        st.write("blabla ")
 
-    elif tab == "Client Ranking":
         # Create columns for layout
-        t1, t2 = st.columns((0.07, 0.5))
+        t1, t2 = st.columns((0.07, 0.35))
 
         # Add logo image to the first column
         with t1:
-            logo_image = "images/index.png"
+            logo_image = "images/image1.png"
             st.image(logo_image, width=120)
 
         # Add title to the second column
         with t2:
-            st.title("CLIENTCO - Clients Analytics Tool")
+            st.title("Welcome to Sales Team Tool - CLIENTCO")
 
-        # Add sentence asking to choose branch
-        st.write("Choose your branch:")
+        intro_text = """
+        This is a churn prevention tool designed to empower sales teams with insights and analytics to proactively identify and address potential customer churn. The application provides sales professionals with the means to forecast which customers are at risk of churning, highlight those whose retention is critical, and strategize on salvaging sales figures by winning back certain customers. The tool features several tabs, each offering unique functionalities:
+
+        - **Client Ranking**: This tab offers a fundamental analysis of customers, ranking them based on various metrics such as churn probability (as determined by our Machine Learning model), total sales return (the total amount spent by the customer on our products), and a priority metric. The priority metric is a formula that uses weighted averages to prioritize some customers over others, based on their potential impact on the business.
+
+        - **Clients Deep-Dive**: This section provides an in-depth analysis of individual customer metrics that are vital for sales teams to understand before making informed decisions. It allows for a detailed examination of the factors influencing customer behavior and churn risk.
+
+        - **Finance Tool**: A practical tool that enables sales teams to input client data and project potential sales recovery based on historical sales performance. This feature assists in financial planning and setting realistic targets for customer retention and sales recovery efforts.
+
+        Sales teams can explore and familiarize themselves with these tabs to make better-informed decisions quickly and efficiently.
+        """
+        
+        st.write(intro_text)
+
+    elif tab == "AI Client Ranking":
+        # Create columns for layout
+        t1, t2 = st.columns((0.07, 0.35))
+
+        # Add logo image to the first column
+        with t1:
+            logo_image = "images/image3.png"
+            st.image(logo_image, width=120)
+
+        # Add title to the second column
+        with t2:
+            st.title("CLIENTCO - Clients Analytics Tool - AI Powered Client Ranking")
+
+        st.write("This tab offers a fundamental analysis of customers, ranking them based on various metrics such as churn probability (as determined by our Machine Learning model), total sales return (the total amount spent by the customer on our products), and a priority metric. The priority metric is a formula that uses weighted averages to prioritize some customers over others, based on their potential impact on the business.")
 
         # Get unique branches from the dataset
         branches = ['All'] + data['branch_id'].unique().tolist()
@@ -61,27 +96,26 @@ def main():
         display_columns = {
             'client_id': 'Client ID',
             'branch_id': 'Branch ID',
-            'importance_score':'Importance Score',
-            'proba_churn': 'Probability of Churn',
+            'priority_score':'Priority Score',
+            'churn_probability': 'Probability of Churn',
             'tot_sales_net': 'Total Net Sales',
             'avg_freq_orders': 'Frequency Orders',
             'lag_day_last': 'Days since Last Order',
-            'nb_ret': 'Number Returns',
-            'quali_relation' : 'Relationship Quality',
-            'pct_online': 'Pct sales online',
-            'pct_store': 'Pct sales in store',
-            'pct_phone': 'Pct sales over the phone',
-            'pct_visits': 'Pct sales during visits',
-            'pct_other': 'Pct sales other',
             'client_type' : 'client_type'
         }
 
         # Apply column renaming and selection
         filtered_data = filtered_data.rename(columns=display_columns)[display_columns.values()]
 
-
-        # Round 'Frequency Orders' to two decimal places
         filtered_data['Frequency Orders'] = filtered_data['Frequency Orders'].round(2)
+
+        filtered_data['Priority Score'] = filtered_data['Priority Score'].round(2)
+
+        filtered_data['Total Net Sales'] = filtered_data['Total Net Sales'].round(2)
+
+        filtered_data['Probability of Churn'] = filtered_data['Probability of Churn']*100
+
+        filtered_data['Probability of Churn'] = filtered_data['Probability of Churn'].round(2)
 
         # Filter clients based on type
         client_types = ['Recurrent', 'Occasional', 'New']
@@ -101,10 +135,10 @@ def main():
         filtered_data = filtered_data.drop(columns=['client_type'])
 
         # Sort data by 'Probability of Churn' and 'Total Sales Net'
-        sort_by = st.selectbox("Sort by", ["", "Importance Score", "Probability of Churn", "Total Net Sales"])
+        sort_by = st.selectbox("Sort by", ["", "Priority Score", "Probability of Churn", "Total Net Sales"])
         if sort_by:
-            if sort_by == "Importance Score":
-                filtered_data = filtered_data.sort_values(by="Importance Score", ascending=False)
+            if sort_by == "Priority Score":
+                filtered_data = filtered_data.sort_values(by="Priority Score", ascending=False)
             elif sort_by == "Probability of Churn":
                 filtered_data = filtered_data.sort_values(by="Probability of Churn", ascending=False)
             elif sort_by == "Total Net Sales":
@@ -114,7 +148,6 @@ def main():
         st.write("Clients of the selected branch:")
         st.write(filtered_data)
 
-    # ... (the rest of your code before the elif statement)
 
 
     elif tab == "Client Deep-dive":
@@ -137,21 +170,7 @@ def main():
                 if not client_data.empty:
                     client_data = client_data.iloc[0]
 
-                    # Client Type mapping
-                    client_type_mapping = {
-                        'recurrent_client': 'Recurrent Client',
-                        'occasional_client': 'Occasional Client',
-                        'new_client': 'New Client'
-                    }
-                    client_data['client_type'] = client_type_mapping.get(client_data['client_type'], client_data['client_type'])
-
-                    col1, col2, col3 = st.columns(3)
-                    with col1:
-                        st.metric("Importance Score", value="{:.2f}".format(client_data['importance_score']))
-                    with col2:
-                        st.metric("Probability of Churn", value="{:.2%}".format(client_data['proba_churn']))
-                    with col3:
-                        st.metric("Total Net Sales", value="${:,.2f}".format(client_data['tot_sales_net']))
+                    # Display metrics as before
 
                     # Detailed Analytics
                     st.subheader("Detailed Analytics")
@@ -175,10 +194,21 @@ def main():
                     }
                     st.table(pd.DataFrame(contact_data, index=[0]))
 
+                    # Plot time series data
+                    st.subheader("Time Series Data")
+                    client_time_data = time_data[time_data['client_id'] == client_id]
+                    if not client_time_data.empty:
+                        fig = px.line(client_time_data, x='date_order', y='sales_net', title=f'Time Series: Sales Net Over Time (Client ID: {client_id})')
+                        st.plotly_chart(fig)
+                    else:
+                        st.warning("No time series data available for this client.")
                 else:
                     st.error("Client ID not found in the dataset.")
             except ValueError:
                 st.error("Client ID must be a number.")
+        
+    elif tab == "Financial Tool":
+        st.header("Financial Tool")
 
 
 
